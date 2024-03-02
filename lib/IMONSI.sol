67fc2eccb6b98a74e3a6ee239529559606cf16fd
The Notorious Monsi <155488833+TheNotoriousMonsi@users.noreply.github.com>
2024-03-02 19:01:40 +0000

    Update IMONSI.sol

--- lib/IMONSI.sol
+++ lib/IMONSI.sol
@@ -2,7 +2,28 @@
 pragma solidity ^0.8.0;
 
 interface IMONSI {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    // ERC-20 standard functions
+    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    // Minting and burning
+    function mint(address to, uint256 amount) external;
+    function burn(address from, uint256 amount) external;
+
+    // Governance
+    function delegate(address delegatee) external;
+    function getVotes(address account) external view returns (uint256);
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Minted(address indexed to, uint256 amount);
+    event Burned(address indexed from, uint256 amount);
+    event Delegation(address indexed delegatee, uint256 indexed delegationType);
+
+    // Add more functions as necessary
 }
